4470e0c002d560a496b261845eb146cb58512571
door0 <moonzerocheo@gmail.com>
2021-03-20 14:21:15 +0900

    2021-03-20-3

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.btn-search').click(function() {
+        console.log('search!');
+    });
+});
+
+var API_URL = 'https://floating-harbor-78336.herokuapp.com/fastfood'
+
+$(function() {
+    $('.btn-search').click(function() {
+        $.get(API_URL, {}, function(data) {
+            console.log(data);
+        });
+    });
+});
