fefdc07b019b03ea0cecad7bc07737fd72cb28ce
door0 <moonzerocheo@gmail.com>
2021-03-21 22:59:23 +0900

    2021-03-21

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//API 이용하여 검색 기능 구현하기 
 $(function() {
     $('.btn-search').click(function() {
         console.log('search!');
@@ -13,3 +14,21 @@ $(function() {
         });
     });
 });
+
+//패스트 푸드점 개수 출력하기 
+$.get(API_URL, {}, function(data){
+    var list = data.list;
+    var total = data.total;
+
+    $('.total').html('총 '+ total +'개의 패스트푸드점을 찾았습니다. ');
+
+    //패스트푸드점 목록 출력하기
+    var $list = $('.list');
+
+    for(var i=0; i< list.length; i++) {
+        var item = list[i];
+        //각 아이템 하나하나마다 DOM객체를 만들어서 $list에 추가한다.
+    }
+});
+
+
